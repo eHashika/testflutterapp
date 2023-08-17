df69710f3b01ef87fe45b20922d540c53583cfed
eHashika <hashika_wde_e21@engug.ruh.ac.lk>
2023-08-17 23:27:19 +0530

    com_2

--- testflutterapp/lib/main.dart
+++ testflutterapp/lib/main.dart
@@ -1,46 +1,104 @@
+import 'dart:async';
+//import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart' as dom;
 
 void main() {
-  runApp(MyApp());
+  runApp(Test());
 }
 
-class MyApp extends StatefulWidget {
+class Test extends StatefulWidget {
+  const Test({Key? key}) : super(key: key);
+
   @override
-  _MyAppState createState() => _MyAppState();
+  _TestState createState() => _TestState();
 }
 
-class _MyAppState extends State<MyApp> {
-  String searchInput = '';
+class _TestState extends State<Test> {
+  List<Article> articles = [];
 
-  Future<void> _scrapeSearchInput() async {
-    var response = await http.get(Uri.parse('https://www.google.lk'));
-    if (response.statusCode == 200) {
-      var document = parse(response.body);
-      var searchInputElement = document.querySelector('input[name="q"]');
-      setState(() {
-        searchInput =
-            searchInputElement?.attributes['value'] ?? 'Input not found';
-      });
-    }
+  @override
+  void initState() {
+    super.initState();
+
+    getWebsiteData();
   }
 
+  Future getWebsiteData() async {
+    final url = Uri.parse('https://www.amazon.com/s?k=iphone');
+    final response = await http.get(url);
+    dom.Document html = dom.Document.html(response.body);
+
+    final titles = html
+        .querySelectorAll('h2 > a > span')
+        .map((element) => element.innerHtml.trim())
+        .toList();
+
+    final urls = html
+        .querySelectorAll('h2 > a')
+        .map(
+            (element) => 'https://www.amazon.com/${element.attributes['href']}')
+        .toList();
+
+    final urlImages = html
+        .querySelectorAll('span > a > div > img')
+        .map((element) => element.attributes['src']!)
+        .toList();
+
+    //print('Count: ${titles , length}');
+
+    setState(() {
+      articles = List.generate(
+        titles.length,
+        (index) => Article(
+          title: titles[index],
+          url: urls[index],
+          urlImage: urlImages[index],
+        ),
+      );
+    });
+  }
+  //     for (final title in titles){
+  //      debugPrint(title);
+  //    }
+//  }
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+  Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: Text('Web Scraping'),
+          title: const Text('Web Scraping'),
+          centerTitle: true,
         ),
-        body: Center(
-          child: Text(searchInput),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(12),
+          itemCount: articles.length,
+          itemBuilder: (context, index) {
+            final article = articles[index];
+
+            return ListTile(
+              leading: Image.network(
+                article.urlImage,
+                width: 50,
+                fit: BoxFit.fitHeight,
+              ),
+              title: Text(article.title),
+              subtitle: Text(article.url),
+            );
+          },
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _scrapeSearchInput,
-          child: Icon(Icons.refresh),
-        ),
-      ),
-    );
-  }
+      );
+}
+
+class Article {
+  final String url;
+  final String title;
+  final String urlImage;
+
+  const Article({
+    required this.url,
+    required this.title,
+    required this.urlImage,
+  });
 }
